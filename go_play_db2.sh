1c318c07049104129cf1555e5cbf06fb5715dd74
Miguel Soranno <msoranno@gmail.com>
2017-06-19 13:36:40 +0200

    Es el script que acompana al playbook play_db2.yml

---
+++ go_play_db2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+	echo
+	echo "Parametros incorrectos, Ejemplo: "
+	echo
+	echo " ./go_play_db2.sh [grupo de maquinas del fichero de inventario] [accion]"
+	echo
+	echo " ./go_play_db2.sh maquinasdb2 STOP"
+	echo " ./go_play_db2.sh maquinasdb2 START"
+	echo " ./go_play_db2.sh maquinasdb2 STATUS"
+	echo
+else
+  echo
+  echo "Datos pasados: "
+  echo "- grupo: $1"
+  echo "- accion: $2"
+  echo 
+  echo
+
+
+  ansible-playbook --extra-vars="GRUPOMAQUINAS=$1 ACCION=$2" play_db2.yml
+fi
+
+
+
